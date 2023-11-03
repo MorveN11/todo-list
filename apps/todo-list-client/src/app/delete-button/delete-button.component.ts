1819579b0877dd3643166c6b698e5f1cf5068c10
fundacion <Severaldog>
2023-11-02 21:32:42 -0400

    feat: remove tasks

--- apps/todo-list-client/src/app/delete-button/delete-button.component.ts
+++ apps/todo-list-client/src/app/delete-button/delete-button.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-delete-button',
   templateUrl: './delete-button.component.html',
 })
-export class DeleteButtonComponent {}
+export class DeleteButtonComponent {
+  @Output() deleteClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  onClick(): void {
+    this.deleteClicked.emit();
+  }
+}
