bca4fd6a83884b5cc27c1d276a124a2f25286c84
MorveN11 <morales.patty.jose@gmail.com>
2023-11-03 13:13:29 -0400

    chore: show swagger in testing

--- apps/todo-list-api/src/main.ts
+++ apps/todo-list-api/src/main.ts
@@ -3,13 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import AppModule from './app.module';
-import { DESCRIPTION, PORT, PREFIX, TITLE, VERSION } from './utils/constants';
+import { DESCRIPTION, NODE_ENV, PORT, PREFIX, TITLE, VERSION } from './utils/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder().setTitle(TITLE).setDescription(DESCRIPTION).setVersion(VERSION).build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(PREFIX, app, document);
+
+  if (NODE_ENV === 'development' || NODE_ENV === 'testing') {
+    const config = new DocumentBuilder().setTitle(TITLE).setDescription(DESCRIPTION).setVersion(VERSION).build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(PREFIX, app, document);
+  }
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(PREFIX);
--- apps/todo-list-api/src/utils/constants.ts
+++ apps/todo-list-api/src/utils/constants.ts
@@ -1,8 +1,9 @@
 const DESCRIPTION =
   'The Todo List API for the 4th semester of the Software Engineering course at the Salesiana University of Bolivia.';
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
 const PORT = process.env.PORT ?? 3000;
 const PREFIX = '/api';
 const TITLE = 'Todo List API';
 const VERSION = '1.0';
 
-export { DESCRIPTION, PORT, PREFIX, TITLE, VERSION };
+export { DESCRIPTION, NODE_ENV, PORT, PREFIX, TITLE, VERSION };
